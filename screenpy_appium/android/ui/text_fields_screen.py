3064877fb3c84cbf4afb9ed2759e1fe9f99382e8
Perry Goy <perry.goy@gmail.com>
2022-05-01 02:24:24 -0500

    add docstrings to Android ui files, this time for real.

--- screenpy_appium/android/ui/text_fields_screen.py
+++ screenpy_appium/android/ui/text_fields_screen.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 """
 The screen that appears after tapping Text Fields in the Views screen.
 """
 
-=======
->>>>>>> 97e2d96c2cc38d10081c4efa593f141fbf697365
 from screenpy_appium import Target
 
 
